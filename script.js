c801f8c0ac7e29cb7526eba2a915116100c91c85
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-08-31 10:46:04 +0200

    Update script.js
    making a better code without repeat of lines

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 
 focoBt.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "foco");
-  banner.setAttribute("src", "./imagens/foco.png");
+  AlterarContexto("foco");
 });
 
 curtoBt.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-curto");
-  banner.setAttribute("src", "./imagens/descanso-curto.png");
+  AlterarContexto("descanso-curto");
 });
 
 longoBt.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-longo");
-  banner.setAttribute("src", "./imagens/descanso-longo.png");
+  AlterarContexto("descanso-longo");
 });
+
+function AlterarContexto(contexto) {
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/imagens/${contexto}.png`);
+}

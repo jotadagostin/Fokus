f01f36b152018e46f5e4c3f1457c7985b666e3a3
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-01 10:34:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,38 +4,46 @@ const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
 
 focoBt.addEventListener("click", () => {
   AlterarContexto("foco");
+  focoBt.classList.add("active");
 });
 
 curtoBt.addEventListener("click", () => {
   AlterarContexto("descanso-curto");
+  curtoBt.classList.add("active");
 });
 
 longoBt.addEventListener("click", () => {
   AlterarContexto("descanso-longo");
+  longoBt.classList.add("active");
 });
 
 function AlterarContexto(contexto) {
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
+  
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `/imagens/${contexto}.png`);
   switch (contexto) {
     case "foco":
       titulo.innerHTML = `
-      Optimize your productivity, <br>
+      O ptimize your productivity, <br>
       <strong class="app_title-strong"> dive in that matters! </strong>
       `;
       break;
-    case "descanso-curto": 
+    case "descanso-curto":
       titulo.innerHTML = `
       How about taking a breath? <strong class="app__title-strong">Take a short break!</strong>
-      `
+      `;
       break;
     case "descanso-longo":
-        titulo.innerHTML = `
+      titulo.innerHTML = `
         Time to get back to the surface. <strong class="app__title-strong"> Take a long break! </strong>
-        `
+        `;
     default:
       break;
   }
